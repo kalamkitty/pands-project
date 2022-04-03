cfdcfa7abd7197e1777f0793d546fe814fbfecc3
kalamkitty <g00411363@gmit.ie>
2022-04-03 14:24:06 +0100

    Calculate mean of each variable

--- analysis.py
+++ analysis.py
@@ -12,19 +12,25 @@ import numpy as np
 # importing the numpy function
 
 
-df = pd.read_csv('iris.csv', header=None)  
+df = pd.read_csv('iris.csv', header = None)  
 # Read CSV data and form a dataframe. Added "header=none" to take the first line of data set into account.
 
 
-df.columns = ["Sepal_length", "Sepal_width", "Petal_length", "Petal_width", "Iris class"] 
+df.columns = ["Sepal length (cm)", "Sepal width (cm)", "Petal length (cm)", "Petal width (cm)", "Class"] 
 # Adding headings to the dataset.
 # If I was to print, dataframe, the data set is too large therefore it will print a summary (first and last 5 rows).
 
+pd.set_option('display.max_rows', 150)
+# setting the max display rows to 150 as there are 150 lines of data.
 
-print(df.to_csv(r'dataSummary.txt',
-                header = df.columns,
-                index = "Dataset",
-                sep= " ",
-                mode = "a"))
-            
+with open('dataSummary.txt', "w") as f:
+     f.write(str(df))
+# open txt file and set to write mode. Write data into txt file.
+
+print ("Mean", df["Sepal length (cm)"].mean())   
+print ("Mean", df["Sepal width (cm)"].mean()) 
+print ("Mean", df["Petal length (cm)"].mean()) 
+print ("Mean", df["Petal width (cm)"].mean())  
+
+
 
